358608879242e5ce00419a9cd8513ff21606e984
Dimitri Sifoua <dimitri.sifoua@gmail.com>
2023-04-30 19:15:30 -0400

    Add ToDo models

---
+++ src/models/ToDo.ts
@@ -0,0 +1,27 @@
+export class ToDoItem {
+    private _id: number
+    private _description: string
+    private _checked: boolean
+
+    constructor(id: number, description: string, checked: boolean = false) {
+        this._id = id
+        this._description = description
+        this._checked = checked
+    }
+
+    public get id() {
+        return this._id
+    }
+
+    public get descripiton() {
+        return this._description
+    }
+
+    public get checked() {
+        return this._checked
+    }
+}
+
+type ToDoList = ToDoItem[]
+
+export default ToDoList
